c7ff7e225445c131dc082a7bdd269d0f9e1d0b02
Asen <asen.trichkov1@gmail.com>
2023-01-06 03:01:06 +0200

    Add Delete option

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ print('========================= Command TODO line tool ========================
 commands = {
     '1': 'Add new todo',
     '2': 'List all todos',
+    '3': 'Delete todo',
 }
 
 # List possible commands
@@ -17,6 +18,10 @@ def loadFileData():
         data = file.read()
         return json.loads(data)
 
+def saveData(data):
+    file = open('todos.json', 'w+')
+    file.write(json.dumps(data))
+
 def addNewTodo():
     try:
         data = loadFileData()
@@ -26,16 +31,18 @@ def addNewTodo():
     todoName = str(input('Enter new todo name: '))
     todoDesc = str(input('Enter new todo description (enter to skip): '))
     # Save data from input
-    file = open('todos.json', 'w+')
     data.append({"name": todoName, "desc": todoDesc})
-    file.write(json.dumps(data))
+    saveData(data)
 
 def listTodos():
     table = PrettyTable()
-    table.field_names = ['Name', 'Description']
+    table.field_names = ['#', 'Name', 'Description']
     todosList = loadFileData()
+
+    index = 1
     for todo in todosList:
-        table.add_row([todo["name"], todo["desc"]])
+        table.add_row([index, todo["name"], todo["desc"]])
+        index += 1
     
     print(table)
 
@@ -45,3 +52,16 @@ if inpCommand == 1:
     addNewTodo()
 elif inpCommand == 2:
     listTodos()
+elif inpCommand == 3:
+    indexTodo = int(input('Select todo number that you want to delete ? '))
+    data = loadFileData()
+
+    if len(data) < indexTodo:
+        print("Sorry, selected todo don't exist")
+
+    counter = 1
+    for x in data:
+        if (counter == indexTodo):
+            del data[counter-1]
+        counter += 1
+    saveData(data)
